9d2fcad69bb249abdfc061d783b81ad24d51229d
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2026-01-21 15:44:23 +0100

    added dashboard page for tasks KPI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {Login} from './features/auth/login/login';
 import {TaskList} from './features/task/task-list/task-list';
 import {authGuard} from './core/auth/guards/auth-guard';
 import {TaskForm} from './features/task/task-form/task-form';
+import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
   {path: 'register', component: Register},
@@ -12,4 +13,5 @@ export const routes: Routes = [
   {path: 'create', component: TaskForm, canActivate: [authGuard]},
   {path: '', redirectTo: '/tasks', pathMatch: 'full'},
   {path: 'edit/:id', component: TaskForm, canActivate: [authGuard]},
+  {path: 'dashboard', component: Dashboard, canActivate: [authGuard]},
 ];
