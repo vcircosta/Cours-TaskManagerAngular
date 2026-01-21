6b016c1ea6a3ed88fd35b5454c87715f4dd03608
William <williamlg@free.fr>
2026-01-21 15:45:50 +0100

    added: tas details

--- src/app/features/task/task-details/task-details.ts
+++ src/app/features/task/task-details/task-details.ts
@@ -1,16 +1,43 @@
-import {Component, inject, Input, signal} from '@angular/core';
+import {Component, DestroyRef, inject, Input, OnInit, signal} from '@angular/core';
 import {TaskService} from '../services/taskService';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {Status, Tasks} from '../interfaces/tasks';
+import {TaskStatusPipe} from '../pipe/task-status-pipe';
 
 @Component({
     selector: 'app-task-details',
-    imports: [],
+    imports: [
+        TaskStatusPipe
+    ],
     templateUrl: './task-details.html',
     styleUrl: './task-details.css',
 })
-export class TaskDetails {
+export class TaskDetails implements OnInit {
     @Input() id!: number;
-
     private taskService: TaskService = inject(TaskService);
+    private destroyRef = inject(DestroyRef);
 
-    currentTask = signal(this.taskService.getTaskById(this.id));
+    currentTask = signal<Tasks | null>(null);
+
+    ngOnInit() {
+        this.getCurrentTask();
+    }
+
+    getCurrentTask () {
+        this.taskService.getTaskById(this.id)
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe({
+                next: (task: Tasks) => this.currentTask.set(task),
+                error: () => console.log('error')
+            })
+    }
+
+    getStatusColor (status: Status | undefined) {
+        switch (status) {
+            case Status.PENDING: return 'bg-red-100 text-red-800'
+            case Status.IN_PROGRESS: return 'bg-amber-100 text-amber-800'
+            case Status.DONE: return 'bg-green-100 text-green-800'
+            default: return 'bg-red-100 text-red-800'
+        }
+    }
 }
