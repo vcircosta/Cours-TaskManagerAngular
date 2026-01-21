54a55facc93d8a3a526bfe81de9f097a73d16de8
William <williamlg@free.fr>
2026-01-21 16:06:20 +0100

    WIP: comments task

---
+++ src/app/features/comments/comments-form/comments-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-form',
+  imports: [],
+  templateUrl: './comments-form.html',
+  styleUrl: './comments-form.css',
+})
+export class CommentsForm {
+
+}
---
+++ src/app/features/comments/comments-list/comments-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-list',
+  imports: [],
+  templateUrl: './comments-list.html',
+  styleUrl: './comments-list.css',
+})
+export class CommentsList {
+
+
+}
